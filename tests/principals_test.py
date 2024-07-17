32f746d6f3ccde2a3209db7d93f4c80f486070b2
Nabajit Das <dasnabajit2005@gmail.com>
2024-07-17 15:35:49 +0530

    "challenge completed"

--- tests/principals_test.py
+++ tests/principals_test.py
@@ -21,7 +21,7 @@ def test_grade_assignment_draft_assignment(client, h_principal):
     response = client.post(
         '/principal/assignments/grade',
         json={
-            'id': 5,
+            'id': 4,
             'grade': GradeEnum.A.value
         },
         headers=h_principal
@@ -34,7 +34,7 @@ def test_grade_assignment(client, h_principal):
     response = client.post(
         '/principal/assignments/grade',
         json={
-            'id': 4,
+            'id': 5,
             'grade': GradeEnum.C.value
         },
         headers=h_principal
@@ -50,7 +50,7 @@ def test_regrade_assignment(client, h_principal):
     response = client.post(
         '/principal/assignments/grade',
         json={
-            'id': 4,
+            'id': 5,
             'grade': GradeEnum.B.value
         },
         headers=h_principal
@@ -60,3 +60,15 @@ def test_regrade_assignment(client, h_principal):
 
     assert response.json['data']['state'] == AssignmentStateEnum.GRADED.value
     assert response.json['data']['grade'] == GradeEnum.B
+
+def test_get_teachers(client, h_principal):
+    response = client.get(
+        'principal/teachers',
+        headers=h_principal
+    )
+
+    assert response.status_code == 200
+
+    data = response.json['data']
+    for teacher in data:
+        assert teacher['id'] is not None
